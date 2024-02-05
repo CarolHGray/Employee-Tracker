928f5055d4658fc7fa87af886b9a6e3dd44d8807
CarolHGray <fivegrays@yahoo.com>
2024-02-05 00:44:57 -0600

    installed console.table

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 // import inquirer 
 const inquirer = require('inquirer'); 
 // import console.table
-const Table = require('console.table'); 
+const cTable = require('console.table'); 
 
 require('dotenv').config()
 
