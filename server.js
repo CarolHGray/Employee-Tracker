07ba9125012985570cef092f725cba545257ae0c
CarolHGray <fivegrays@yahoo.com>
2024-02-05 00:38:57 -0600

    corrected typo in server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 // import inquirer 
 const inquirer = require('inquirer'); 
 // import console.table
-const cTable = require('console.table'); 
+const Table = require('console.table'); 
 
 require('dotenv').config()
 
